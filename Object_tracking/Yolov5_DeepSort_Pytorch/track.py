427af84a855570c843b3e6728e6e4596a3c50a3e
VinitAwale <awale.vinit@gmail.com>
2021-09-25 21:29:48 +0530

    update track.py

--- Object_tracking/Yolov5_DeepSort_Pytorch/track.py
+++ Object_tracking/Yolov5_DeepSort_Pytorch/track.py
@@ -1,10 +1,17 @@
 import sys
-sys.path.insert(0, './yolov5')
+
+sys.path.insert(0, "./yolov5")
 
 from yolov5.utils.google_utils import attempt_download
 from yolov5.models.experimental import attempt_load
 from yolov5.utils.datasets import LoadImages, LoadStreams
-from yolov5.utils.general import check_img_size, non_max_suppression, scale_coords, check_imshow, xyxy2xywh
+from yolov5.utils.general import (
+    check_img_size,
+    non_max_suppression,
+    scale_coords,
+    check_imshow,
+    xyxy2xywh,
+)
 from yolov5.utils.torch_utils import select_device, time_synchronized
 from yolov5.utils.plots import plot_one_box
 from deep_sort_pytorch.utils.parser import get_config
@@ -19,6 +26,8 @@ import cv2
 import torch
 import torch.backends.cudnn as cudnn
 
+fps_values = []  # FPS values
+
 
 def compute_color_for_id(label):
     """
@@ -31,21 +40,49 @@ def compute_color_for_id(label):
 
 
 def detect(opt):
-    out, source, yolo_weights, deep_sort_weights, show_vid, save_vid, save_txt, imgsz, evaluate = \
-        opt.output, opt.source, opt.yolo_weights, opt.deep_sort_weights, opt.show_vid, opt.save_vid, \
-            opt.save_txt, opt.img_size, opt.evaluate
-    webcam = source == '0' or source.startswith(
-        'rtsp') or source.startswith('http') or source.endswith('.txt')
+    (
+        out,
+        source,
+        yolo_weights,
+        deep_sort_weights,
+        show_vid,
+        save_vid,
+        save_txt,
+        imgsz,
+        evaluate,
+    ) = (
+        opt.output,
+        opt.source,
+        opt.yolo_weights,
+        opt.deep_sort_weights,
+        opt.show_vid,
+        opt.save_vid,
+        opt.save_txt,
+        opt.img_size,
+        opt.evaluate,
+    )
+    webcam = (
+        source == "0"
+        or source.startswith("rtsp")
+        or source.startswith("http")
+        or source.endswith(".txt")
+    )
 
     # initialize deepsort
     cfg = get_config()
     cfg.merge_from_file(opt.config_deepsort)
-    attempt_download(deep_sort_weights, repo='mikel-brostrom/Yolov5_DeepSort_Pytorch')
-    deepsort = DeepSort(cfg.DEEPSORT.REID_CKPT,
-                        max_dist=cfg.DEEPSORT.MAX_DIST, min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE,
-                        nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
-                        max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
-                        use_cuda=True)
+    attempt_download(deep_sort_weights, repo="mikel-brostrom/Yolov5_DeepSort_Pytorch")
+    deepsort = DeepSort(
+        cfg.DEEPSORT.REID_CKPT,
+        max_dist=cfg.DEEPSORT.MAX_DIST,
+        min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE,
+        nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP,
+        max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
+        max_age=cfg.DEEPSORT.MAX_AGE,
+        n_init=cfg.DEEPSORT.N_INIT,
+        nn_budget=cfg.DEEPSORT.NN_BUDGET,
+        use_cuda=True,
+    )
 
     # Initialize
     device = select_device(opt.device)
@@ -57,13 +94,15 @@ def detect(opt):
             pass
             shutil.rmtree(out)  # delete output folder
         os.makedirs(out)  # make new output folder
-    half = device.type != 'cpu'  # half precision only supported on CUDA
+    half = device.type != "cpu"  # half precision only supported on CUDA
 
     # Load model
     model = attempt_load(yolo_weights, map_location=device)  # load FP32 model
     stride = int(model.stride.max())  # model stride
     imgsz = check_img_size(imgsz, s=stride)  # check img_size
-    names = model.module.names if hasattr(model, 'module') else model.names  # get class names
+    names = (
+        model.module.names if hasattr(model, "module") else model.names
+    )  # get class names
     if half:
         model.half()  # to FP16
 
@@ -80,17 +119,19 @@ def detect(opt):
         dataset = LoadImages(source, img_size=imgsz)
 
     # Get names and colors
-    names = model.module.names if hasattr(model, 'module') else model.names
+    names = model.module.names if hasattr(model, "module") else model.names
 
     # Run inference
-    if device.type != 'cpu':
-        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    if device.type != "cpu":
+        model(
+            torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters()))
+        )  # run once
     t0 = time.time()
 
     save_path = str(Path(out))
     # extract what is in between the last '/' and last '.'
-    txt_file_name = source.split('/')[-1].split('.')[0]
-    txt_path = str(Path(out)) + '/' + txt_file_name + '.txt'
+    txt_file_name = source.split("/")[-1].split(".")[0]
+    txt_path = str(Path(out)) + "/" + txt_file_name + ".txt"
 
     for frame_idx, (path, img, im0s, vid_cap) in enumerate(dataset):
         img = torch.from_numpy(img).to(device)
@@ -105,28 +146,32 @@ def detect(opt):
 
         # Apply NMS
         pred = non_max_suppression(
-            pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+            pred,
+            opt.conf_thres,
+            opt.iou_thres,
+            classes=opt.classes,
+            agnostic=opt.agnostic_nms,
+        )
         t2 = time_synchronized()
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
             if webcam:  # batch_size >= 1
-                p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
+                p, s, im0 = path[i], "%g: " % i, im0s[i].copy()
             else:
-                p, s, im0 = path, '', im0s
+                p, s, im0 = path, "", im0s
 
-            s += '%gx%g ' % img.shape[2:]  # print string
+            s += "%gx%g " % img.shape[2:]  # print string
             save_path = str(Path(out) / Path(p).name)
 
             if det is not None and len(det):
                 # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(
-                    img.shape[2:], det[:, :4], im0.shape).round()
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
                 # Print results
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
-                    s += '%g %ss, ' % (n, names[int(c)])  # add to string
+                    s += "%g %ss, " % (n, names[int(c)])  # add to string
 
                 xywhs = xyxy2xywh(det[:, 0:4])
                 confs = det[:, 4]
@@ -134,19 +179,21 @@ def detect(opt):
 
                 # pass detections to deepsort
                 outputs = deepsort.update(xywhs.cpu(), confs.cpu(), clss, im0)
-                
+
                 # draw boxes for visualization
                 if len(outputs) > 0:
-                    for j, (output, conf) in enumerate(zip(outputs, confs)): 
-                        
+                    for j, (output, conf) in enumerate(zip(outputs, confs)):
+
                         bboxes = output[0:4]
                         id = output[4]
                         cls = output[5]
 
                         c = int(cls)  # integer class
-                        label = f'{id} {names[c]} {conf:.2f}'
+                        label = f"{id} {names[c]} {conf:.2f}"
                         color = compute_color_for_id(id)
-                        plot_one_box(bboxes, im0, label=label, color=color, line_thickness=2)
+                        plot_one_box(
+                            bboxes, im0, label=label, color=color, line_thickness=2
+                        )
 
                         if save_txt:
                             # to MOT format
@@ -155,26 +202,47 @@ def detect(opt):
                             bbox_w = output[2] - output[0]
                             bbox_h = output[3] - output[1]
                             # Write MOT compliant results to file
-                            with open(txt_path, 'a') as f:
-                               f.write(('%g ' * 10 + '\n') % (frame_idx, id, bbox_left,
-                                                           bbox_top, bbox_w, bbox_h, -1, -1, -1, -1))  # label format
+                            with open(txt_path, "a") as f:
+                                f.write(
+                                    ("%g " * 10 + "\n")
+                                    % (
+                                        frame_idx,
+                                        id,
+                                        bbox_left,
+                                        bbox_top,
+                                        bbox_w,
+                                        bbox_h,
+                                        -1,
+                                        -1,
+                                        -1,
+                                        -1,
+                                    )
+                                )  # label format
 
             else:
                 deepsort.increment_ages()
 
             # Print time (inference + NMS)
-            print('%sDone. (%.3fs)' % (s, t2 - t1))
+            print("%sDone. (%.3fs)" % (s, t2 - t1))
 
-            # print fps on screen 
+            # print fps on screen
             fps = 0.0
-            fps  = ( fps + (1./(time.time()-t1)) ) / 2
-            cv2.putText(im0, "FPS: {:.2f}".format(fps), (0, 30),cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
-
+            fps = (fps + (1.0 / (time.time() - t1))) / 2
+            cv2.putText(
+                im0,
+                "FPS: {:.2f}".format(fps),
+                (0, 30),
+                cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                1,
+                (0, 0, 255),
+                2,
+            )
+            fps_values.append(fps)
 
             # Stream results
             if show_vid:
                 cv2.imshow(p, im0)
-                if cv2.waitKey(1) == ord('q'):  # q to quit
+                if cv2.waitKey(1) == ord("q"):  # q to quit
                     raise StopIteration
 
             # Save results (image with detections)
@@ -189,42 +257,91 @@ def detect(opt):
                         h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
                     else:  # stream
                         fps, w, h = 30, im0.shape[1], im0.shape[0]
-                        save_path += '.mp4'
+                        save_path += ".mp4"
 
-                    vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                    vid_writer = cv2.VideoWriter(
+                        save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h)
+                    )
                 vid_writer.write(im0)
 
     if save_txt or save_vid:
-        print('Results saved to %s' % os.getcwd() + os.sep + out)
-        if platform == 'darwin':  # MacOS
-            os.system('open ' + save_path)
+        print("Results saved to %s" % os.getcwd() + os.sep + out)
+        if platform == "darwin":  # MacOS
+            os.system("open " + save_path)
 
-    print('Done. (%.3fs)' % (time.time() - t0))
+    print("Done. (%.3fs)" % (time.time() - t0))
+    return fps_values
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--yolo_weights', type=str, default='yolov5/weights/yolov5s.pt', help='model.pt path')
-    parser.add_argument('--deep_sort_weights', type=str, default='deep_sort_pytorch/deep_sort/deep/checkpoint/ckpt.t7', help='ckpt.t7 path')
+    parser.add_argument(
+        "--yolo_weights",
+        type=str,
+        default="yolov5/weights/yolov5s.pt",
+        help="model.pt path",
+    )
+    parser.add_argument(
+        "--deep_sort_weights",
+        type=str,
+        default="deep_sort_pytorch/deep_sort/deep/checkpoint/ckpt.t7",
+        help="ckpt.t7 path",
+    )
     # file/folder, 0 for webcam
-    parser.add_argument('--source', type=str, default='0', help='source')
-    parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
-    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--show-vid', action='store_true', help='display tracking video results')
-    parser.add_argument('--save-vid', action='store_true', help='save video tracking results')
-    parser.add_argument('--save-txt', action='store_true', help='save MOT compliant results to *.txt')
+    parser.add_argument("--source", type=str, default="0", help="source")
+    parser.add_argument(
+        "--output", type=str, default="inference/output", help="output folder"
+    )  # output folder
+    parser.add_argument(
+        "--img-size", type=int, default=640, help="inference size (pixels)"
+    )
+    parser.add_argument(
+        "--conf-thres", type=float, default=0.4, help="object confidence threshold"
+    )
+    parser.add_argument(
+        "--iou-thres", type=float, default=0.5, help="IOU threshold for NMS"
+    )
+    parser.add_argument(
+        "--fourcc",
+        type=str,
+        default="mp4v",
+        help="output video codec (verify ffmpeg support)",
+    )
+    parser.add_argument(
+        "--device", default="", help="cuda device, i.e. 0 or 0,1,2,3 or cpu"
+    )
+    parser.add_argument(
+        "--show-vid", action="store_true", help="display tracking video results"
+    )
+    parser.add_argument(
+        "--save-vid", action="store_true", help="save video tracking results"
+    )
+    parser.add_argument(
+        "--save-txt", action="store_true", help="save MOT compliant results to *.txt"
+    )
     # class 0 is person, 1 is bycicle, 2 is car... 79 is oven
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 16 17')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--evaluate', action='store_true', help='augmented inference')
-    parser.add_argument("--config_deepsort", type=str, default="deep_sort_pytorch/configs/deep_sort.yaml")
+    parser.add_argument(
+        "--classes",
+        nargs="+",
+        type=int,
+        help="filter by class: --class 0, or --class 16 17",
+    )
+    parser.add_argument(
+        "--agnostic-nms", action="store_true", help="class-agnostic NMS"
+    )
+    parser.add_argument("--augment", action="store_true", help="augmented inference")
+    parser.add_argument("--evaluate", action="store_true", help="augmented inference")
+    parser.add_argument(
+        "--config_deepsort",
+        type=str,
+        default="deep_sort_pytorch/configs/deep_sort.yaml",
+    )
     args = parser.parse_args()
     args.img_size = check_img_size(args.img_size)
 
     with torch.no_grad():
-        detect(args)
+        fps = detect(args)
+        # write fps to txt file
+        with open("fps.txt", "a") as f:
+            for fps_value in fps:
+                f.write(f'{fps_value:.2f}\n')
